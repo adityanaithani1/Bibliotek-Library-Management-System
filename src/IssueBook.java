77d534713447954613054d4c91ba58ec13fb1db9
adityanaithani1 <aditya.24scse1180455@galgotiasuniversity.ac.in>
2025-06-10 15:26:56 +0530

    Update IssueBook.java

--- src/IssueBook.java
+++ src/IssueBook.java
@@ -17,7 +17,7 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author Sudhir Kushwaha
+ * @author Aditya Naithani
  */
 public class IssueBook extends javax.swing.JFrame {
         PreparedStatement pst;
